8c63a396ac07f8fcdc861465ff5a7bbcec7b065b
urosjarc <jar.fmf@gmail.com>
2019-09-06 23:27:42 +0200

    Adding questions for versions.

--- generators/kotlin/index.js
+++ generators/kotlin/index.js
@@ -17,7 +17,42 @@ module.exports = generator.Base.extend({
 				]
 			},
 			module: {
-				module_sample: []
+				android: [
+					{
+						type: 'input',
+						name: 'android_gradle_version',
+						message: 'Android build Gradle version:',
+						default: '3.4.0-rc03'
+					}
+				],
+				desktop: [
+					{
+						type: 'input',
+						name: 'fx_jvm_version',
+						message: 'JavaFx JVM version:',
+						default: '12'
+					},
+					{
+						type: 'input',
+						name: 'tornado_version',
+						message: 'Tornado version:',
+						default: '1.7.17'
+					}
+				],
+				server: [
+					{
+						type: 'input',
+						name: 'ktor_version',
+						message: 'Ktor version:',
+						default: '1.2.4'
+					},
+					{
+						type: 'input',
+						name: 'logback_version',
+						message: 'Logback version:',
+						default: '1.2.3'
+					}
+				]
 			}
 		};
 	},
@@ -25,11 +60,24 @@ module.exports = generator.Base.extend({
 	_base_clean: function () {
 		sh.echo("\n> ./gradlew wrapper ...\n")
 		sh.exec("./gradlew wrapper");
-
-		sh.echo("\n> ./gradlew check ...\n")
-		sh.exec("./gradlew check");
 	},
 
-	_module_module_sample: function () {
+	_check: function(module){
+		sh.echo("\n> ./gradlew check ...\n")
+		sh.exec("./gradlew check");
 	}
+
+	_module_adapter: function () {
+		this._check();
+	}
+	_module_android: function () {
+		this._check();
+	}
+	_module_server: function () {
+		this._check();
+	}
+	_module_desktop: function () {
+		this._check();
+	}
+
 }).extend(subgenerator);
